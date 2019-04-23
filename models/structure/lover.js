84075aa76410be9677c3c6cb4375d9c79acbf0e2
JustinJia <18202877938@163.com>
2019-04-23 21:43:16 +0800

    update

--- models/structure/lover.js
+++ models/structure/lover.js
@@ -1,4 +1,42 @@
 const defaultFields = require("./defaultFields.js");
 
 exports.fields = Object.assign(
-{}, defaultFields);
+{
+  "created_at": {
+    "type": "datetime",
+    "description": "",
+    "base": true
+  },
+  "id": {
+    "type": "id",
+    "description": ""
+  },
+  "man_id": {
+    "type": "number",
+    "description": ""
+  },
+  "woman_id": {
+    "type": "number",
+    "description": ""
+  },
+  "lover_date": {
+    "type": "date",
+    "description": ""
+  },
+  "wedding_date": {
+    "type": "date",
+    "description": ""
+  },
+  "is_closed": {
+    "type": "number",
+    "description": ""
+  },
+  "is_deleted": {
+    "type": "number",
+    "description": ""
+  },
+  "update_at": {
+    "type": "datetime",
+    "description": ""
+  }
+}, defaultFields);
--- models/structure/loverThing.js
+++ models/structure/loverThing.js
@@ -1,4 +1,43 @@
 const defaultFields = require("./defaultFields.js");
 
 exports.fields = Object.assign(
-{}, defaultFields);
+{
+  "created_at": {
+    "type": "datetime",
+    "description": "",
+    "base": true
+  },
+  "id": {
+    "type": "id",
+    "description": ""
+  },
+  "lover_id": {
+    "type": "number",
+    "description": ""
+  },
+  "user_id": {
+    "type": "number",
+    "description": ""
+  },
+  "occur_date": {
+    "type": "date",
+    "description": "发生时间"
+  },
+  "content": {
+    "type": "string",
+    "description": "",
+    "max": 255
+  },
+  "order_num": {
+    "type": "number",
+    "description": ""
+  },
+  "is_deleted": {
+    "type": "number",
+    "description": ""
+  },
+  "update_at": {
+    "type": "datetime",
+    "description": ""
+  }
+}, defaultFields);
